c734b245d7c682dad5e9521f6c85aa6510a70fa2
taku <taku20030819gmail.com>
2022-08-28 10:59:14 +0900

    ✨ App.vueにcontainerとcountdown-frameの追加

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="container">
+
+    <div class="countdown-frame">
+      test
+    </div>
+    <router-view class="router-view"/>
+  </div>
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -14,6 +16,23 @@
   text-align: center;
   color: #2c3e50;
 }
+.container{
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+}
+.countdown-frame{
+  position: absolute;
+  background: red;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  width: fit-content;
+  height: fit-content;
+}
+
 
 nav {
   padding: 30px;

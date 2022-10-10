710ca44b3757c60be51a783edcc94531551e5c04
Shion Ichikawa <shion1305@gmail.com>
2022-10-11 08:29:24 +0900

    ➕ Vue final modalの追加

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Particles from 'vue3-particles'
+import { vfmPlugin } from 'vue-final-modal'
 
-createApp(App).use(store).use(Particles).use(router).mount('#app')
+createApp(App).use(store).use(Particles).use(router).use(vfmPlugin).mount('#app')
